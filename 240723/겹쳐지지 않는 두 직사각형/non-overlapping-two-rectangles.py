013f03dc4de7b9e5ccb90e80918206e5187a71ac
안상준 <69718646+sangjun19@users.noreply.github.com>
2024-07-23 16:56:09 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240723/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
@@ -0,0 +1,42 @@
+def rectangle(sy, sx, ey, ex, arr):
+    sum = 0
+    for i in range(sy, ey + 1):
+        for j in range(sx, ex + 1):
+            sum += arr[i][j]
+    return sum
+
+def pick_pos(n, m, ay, ax, by, bx, arr):
+    value = float("-inf")
+    for i in range(ay, ay + n):
+        for j in range(ax, ax + m):
+            if i >= n or j >= m:
+                continue
+            if j >= bx:
+                continue
+            for k in range(by, by + n):
+                for l in range(bx, bx + m):
+                    if k >= n or l >= m:
+                        continue            
+                    v1 = rectangle(ay, ax, i, j, arr)
+                    v2 = rectangle(by, bx, k, l, arr)
+                    value = max(value, v1 + v2)
+    return value
+
+def main():
+    n, m = map(int, input().split())
+    arr = []
+    for _ in range(n):
+        arr.append(list(map(int, input().split())))
+    result = float("-inf")
+    for i in range(n):
+        for j in range(m - 1):
+            for k in range(i, n):
+                for l in range(j + 1, m):
+                    # print(i, j, k, l)
+                    sum = pick_pos(n, m, i, j, k, l, arr)
+                    result = max(result, sum)
+
+    print(result)
+
+if __name__ == "__main__":
+    main()
