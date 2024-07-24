3fa9b74d3d5c3ea15e2183c54f1c994ab39842b8
안상준 <69718646+sangjun19@users.noreply.github.com>
2024-07-24 10:33:42 +0900

    Title: 컨베이어 벨트, Time: 180ms, Memory: 25MB, Status: Passed - CodeTree

--- 240724/컨베이어 벨트/conveyor-belt.py
+++ 240724/컨베이어 벨트/conveyor-belt.py
@@ -4,13 +4,14 @@ def main():
     arr.append(list(map(int, input().split())))    
     arr.append(list(map(int, input().split())))    
     
-    temp0 = arr[0][n - 1]
-    temp1 = arr[1][n - 1]
-    for i in range(n - 1, 0, -1):
-        arr[0][i] = arr[0][i - 1]
-        arr[1][i] = arr[1][i - 1]        
-    arr[0][0] = temp1
-    arr[1][0] = temp0
+    for k in range(t):
+        temp0 = arr[0][n - 1]
+        temp1 = arr[1][n - 1]
+        for i in range(n - 1, 0, -1):
+            arr[0][i] = arr[0][i - 1]
+            arr[1][i] = arr[1][i - 1]        
+        arr[0][0] = temp1
+        arr[1][0] = temp0
     
     for value in arr:
         for i in value:
