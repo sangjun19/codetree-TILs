4bca64460c21ca50867ea1f2aaca5da6ed501659
안상준 <69718646+sangjun19@users.noreply.github.com>
2024-07-24 09:56:18 +0900

    Title: 양수 직사각형의 최대 크기, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240724/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -0,0 +1,33 @@
+def calc(arr, i, iv, j, jv):
+    sum = 0
+    for y in range(i, i + iv + 1):
+        for x in range(j, j + jv + 1):
+            if arr[y][x] >= 0:
+                sum += 1
+            else: 
+                return -1
+    return sum
+
+def find_rect(arr, i, j):
+    result = 0
+    for y in range(len(arr)):
+        for x in range(len(arr[0])):
+            if i + y >= len(arr) or j + x >= len(arr[0]):
+                continue
+            result = max(result, calc(arr, i, y, j, x))
+            
+    return result
+
+def main():
+    n, m = map(int, input().split())
+    arr = []
+    result = 0
+    for _ in range(n):
+        arr.append(list(map(int, input().split())))
+    for i in range(n):
+        for j in range(m):
+            result = max(result, find_rect(arr, i, j))
+    print(result)
+    
+if __name__ == "__main__":
+    main()
