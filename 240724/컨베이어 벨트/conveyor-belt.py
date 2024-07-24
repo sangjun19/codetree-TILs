d9c988f6ad0b7ee99dd9bdd2e34c13cb193cd0a4
안상준 <69718646+sangjun19@users.noreply.github.com>
2024-07-24 10:32:40 +0900

    Title: 컨베이어 벨트, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240724/컨베이어 벨트/conveyor-belt.py
@@ -0,0 +1,21 @@
+def main():
+    n, t = map(int, input().split())
+    arr = []
+    arr.append(list(map(int, input().split())))    
+    arr.append(list(map(int, input().split())))    
+    
+    temp0 = arr[0][n - 1]
+    temp1 = arr[1][n - 1]
+    for i in range(n - 1, 0, -1):
+        arr[0][i] = arr[0][i - 1]
+        arr[1][i] = arr[1][i - 1]        
+    arr[0][0] = temp1
+    arr[1][0] = temp0
+    
+    for value in arr:
+        for i in value:
+            print(i, end=" ")
+        print()
+    
+if __name__ == "__main__":
+    main()
