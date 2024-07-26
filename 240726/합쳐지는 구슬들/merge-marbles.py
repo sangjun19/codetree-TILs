03724d3958dd91988196ec1e02d9897258365f10
안상준 <69718646+sangjun19@users.noreply.github.com>
2024-07-26 16:37:29 +0900

    Title: 합쳐지는 구슬들, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240726/합쳐지는 구슬들/merge-marbles.py
@@ -0,0 +1,63 @@
+class Marble:
+    def __init__(self, x, y, weight, dir):
+        self.x = x
+        self.y = y
+        self.weight = weight
+        self.dir = dir
+        
+def check_conflict(marbleList, n):
+    marble_map = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(len(marbleList)):
+        if marbleList[i].weight == 0: continue
+        x, y = marbleList[i].x, marbleList[i].y
+        if marble_map[x][y] == 0:
+            marble_map[x][y] = i + 1
+        else:
+            if marbleList[marble_map[x][y] - 1].weight > marbleList[i].weight: # 원래 있던 구슬이 더 무겁거나 / 같은데 현재 번호가 더 낮은 경우
+                marbleList[marble_map[x][y] - 1].weight += marbleList[i].weight
+                marbleList[i].weight = 0
+            else: # 새로운 구슬이 더 무겁거나 / 같은데 현재 번호가 더 높은 경우
+                marbleList[i].weight += marbleList[marble_map[x][y] - 1].weight        
+                marbleList[marble_map[x][y] - 1].weight = 0
+                marble_map[x][y] = i + 1
+    return marbleList
+
+def print_result(marbleList):
+    max_weight = 0
+    marble_count = 0
+    for marble in marbleList:
+        max_weight = max(max_weight, marble.weight)
+        if marble.weight != 0:
+            marble_count += 1
+    print(marble_count, max_weight)
+    
+        
+def move_marble(marbleList, n, time):
+    dir = [[0, -1], [-1, 0], [0, 1], [1, 0]]
+    for t in range(time):
+        for marble in marbleList:
+            x, y = marble.x, marble.y
+            x += dir[marble.dir][0]
+            y += dir[marble.dir][1]
+            if x < 0 or x >= n or y < 0 or y >= n:
+                x -= dir[marble.dir][0]
+                y -= dir[marble.dir][1]
+                marble.dir = (marble.dir + 2) % 4
+            marble.x, marble.y = x, y
+        marbleList = check_conflict(marbleList, n)
+    print_result(marbleList)
+
+def main():
+    n, m, t = map(int, input().split())
+    marbleList = []
+    for _ in range(m):
+        x, y, dir, weight = input().split()
+        if dir == 'U': d = 0
+        elif dir == 'D': d = 2
+        elif dir == 'L': d = 1
+        elif dir == 'R': d = 3
+        marbleList.append(Marble(int(x) - 1, int(y) - 1, int(weight), d))
+    move_marble(marbleList, n, t)
+    
+if __name__ == "__main__":
+    main()
