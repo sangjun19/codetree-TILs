a9da15383f9ef0034371ee0e1069bfe872b30682
안상준 <69718646+sangjun19@users.noreply.github.com>
2024-08-01 17:30:47 +0900

    Title: 순서대로 구구단, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240801/순서대로 구구단/multiplication-table-in-order.py
+++ 240801/순서대로 구구단/multiplication-table-in-order.py
@@ -3,10 +3,10 @@ def main():
     for i in range(1, 10):
         if a < b:
             for j in range(a, b + 1):
-                print(f"{j} * {i} = {j * i}", end="    ")
+                print(f"{j} * {i} = {j * i}", end="  ")
         else:
             for j in range(a, b - 1, -1):
-                print(f"{j} * {i} = {j * i}", end="    ")
+                print(f"{j} * {i} = {j * i}", end="  ")
         print()
 
 if __name__ == "__main__":
