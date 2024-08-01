0d187cc17140dc29dad88afd00c90c4ef599d062
안상준 <69718646+sangjun19@users.noreply.github.com>
2024-08-01 17:25:25 +0900

    Title: 순서대로 구구단, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240801/순서대로 구구단/multiplication-table-in-order.py
+++ 240801/순서대로 구구단/multiplication-table-in-order.py
@@ -2,7 +2,7 @@ def main():
     a, b = map(int, input().split())
     for i in range(1, 10):
         print(f"{a} * {i} = {a * i}", end='  ')
-        print(f"{b} * {i} = {a * i}", end='  ')
+        print(f"{b} * {i} = {b * i}", end='  ')
         print()
 
 if __name__ == "__main__":
