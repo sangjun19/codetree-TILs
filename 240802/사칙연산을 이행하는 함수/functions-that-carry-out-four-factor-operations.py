ab3c60b41e834a2dbf0171ef002f5a87f777c35f
안상준 <69718646+sangjun19@users.noreply.github.com>
2024-08-02 16:46:17 +0900

    Title: 사칙연산을 이행하는 함수, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240802/사칙연산을 이행하는 함수/functions-that-carry-out-four-factor-operations.py
@@ -0,0 +1,13 @@
+arr = list(input().split())
+result = 0
+x, y = int(arr[0]), int(arr[2])
+if arr[1] == '+':
+    result = x + y
+elif arr[1] == '-':
+    result = x - y
+elif arr[1] == '*':
+    result = x * y
+else:
+    result = x / y
+
+print(f'{x} {arr[1]} {y} = {result}')
