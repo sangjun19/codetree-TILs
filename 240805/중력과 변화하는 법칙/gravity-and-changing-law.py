3bfa6174c1851413bdb8cb3074652f675db0a08e
안상준 <69718646+sangjun19@users.noreply.github.com>
2024-08-05 10:48:16 +0900

    Title: 중력과 변화하는 법칙, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240805/중력과 변화하는 법칙/gravity-and-changing-law.py
@@ -0,0 +1,73 @@
+def gravity(arr, y, x, dir):
+    ny = y
+    while True:
+        ny += dir
+        if ny < 0 or ny >= len(arr):
+            return -1
+        if arr[ny][x] == 1:
+            return ny - dir
+        if arr[ny][x] == 3:
+            return ny
+        
+def bfs(arr, y, x):
+    queue = []
+    visited = []
+    gravity_dir = 1
+    gravity_cnt = 0
+    queue.append((y, x))
+    visited.append((y, x))
+    while queue:
+        y, x = queue.pop(0)        
+        for dy, dx in [[0, 1], [0, -1], [0, 0]]:
+            ny = y + dy
+            nx = x + dx
+            if dy == 0 and dx == 0:
+                gravity_dir *= -1
+                gravity_cnt += 1
+                
+            elif nx < 0 or nx >= len(arr[0]):
+                continue
+            
+            if arr[ny][nx] == 1:
+                continue            
+            ny = gravity(arr, ny, nx, gravity_dir)
+            if ny == -1:
+                continue
+            if (ny, nx) in visited:
+                continue
+            
+            if arr[ny][nx] == 3:
+                return gravity_cnt
+            
+            queue.append((ny, nx))
+            visited.append((ny, nx))
+    return -1
+
+def main():
+    n, m = map(int, input().split())
+    arr = []
+    for _ in range(n):
+        temp = list(input())
+        for i in range(len(temp)):
+            if temp[i] == '#':
+                temp[i] = 1
+            elif temp[i] == '.':
+                temp[i] = 0
+            elif temp[i] == 'C':
+                temp[i] = 2
+            else:
+                temp[i] = 3
+        arr.append(temp)
+    
+    # print(gravity(arr, 1, 1, -1))
+    
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if arr[i][j] == 2:
+                print(bfs(arr, i, j))
+    
+    # for a in arr:
+    #     print(a)
+        
+if __name__ == "__main__":
+    main()
