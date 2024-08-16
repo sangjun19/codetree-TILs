17a6717b1271e31591f887af5f82d9cf1d1e96ac
안상준 <69718646+sangjun19@users.noreply.github.com>
2024-08-16 16:18:49 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 130ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240816/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -0,0 +1,18 @@
+def main():
+    n = int(input())
+    arr = []
+    for _ in range(n):
+        x1, x2 = map(int, input().split())
+        arr.append((x1, x2))
+    dp = [1] * n
+    for i in range(1, n):
+        for j in range(0, i):
+            px1, px2 = arr[j]
+            x1, x2 = arr[i]
+            if x1 > px2:
+                dp[i] += 1
+    print(max(dp))
+            
+    
+if __name__ == "__main__":
+    main()
