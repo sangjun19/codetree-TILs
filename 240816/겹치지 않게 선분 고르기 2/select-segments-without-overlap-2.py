a0e40c221a78c15b5f0d23746a749ba2b26cd0bd
안상준 <69718646+sangjun19@users.noreply.github.com>
2024-08-16 16:39:11 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 195ms, Memory: 25MB, Status: Passed - CodeTree

--- 240816/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
+++ 240816/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -7,9 +7,10 @@ def main():
     arr.sort(key=lambda x: x[1])    
     dp = [1] * n
     for i in range(1, n):
+        cnt = 0
         for j in range(0, i):
             if arr[j][1] < arr[i][0]:
-                dp[i] = dp[j] + 1
+                dp[i] = max(dp[i], dp[j] + 1)
     print(max(dp))
     
 if __name__ == "__main__":
