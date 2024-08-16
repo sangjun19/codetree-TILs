e2cbf5b21c37d4b88745f3f630b8dca222e9c013
안상준 <69718646+sangjun19@users.noreply.github.com>
2024-08-16 15:35:01 +0900

    Title: 2차원 최대 증가 수열, Time: 185ms, Memory: 25MB, Status: Passed - CodeTree

--- 240816/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 240816/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -3,11 +3,14 @@ def main():
     arr = []
     for _ in range(n):
         arr.append(list(map(int, input().split())))
-    dp = [[1] * m for _ in range(n)]
+    dp = [[0] * m for _ in range(n)]
+    dp[0][0] = 1
     for i in range(1, n):
         for j in range(1, m):
             for y in range(0, i):
                 for x in range(0, j):
+                    if dp[y][x] == 0:
+                        continue
                     if arr[y][x] < arr[i][j]:
                         dp[i][j] = max(dp[i][j], dp[y][x] + 1)
     # for d in dp:
