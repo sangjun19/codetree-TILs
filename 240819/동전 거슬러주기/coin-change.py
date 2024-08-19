9448ba66f31af24e7fdb155289b0ceb74766ac90
안상준 <69718646+sangjun19@users.noreply.github.com>
2024-08-19 10:50:20 +0900

    Title: 동전 거슬러주기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240819/동전 거슬러주기/coin-change.py
@@ -0,0 +1,12 @@
+def main():
+    n, m = map(int, input().split())
+    arr = list(map(int, input().split()))
+    dp = [float('inf')] * (m + 1)
+    dp[0] = 0
+    for i in range(1, m + 1):
+        for coin in arr:
+            if i - coin >= 0:
+                dp[i] = min(dp[i - coin] + 1, dp[i])
+    print(max(dp))
+if __name__ == "__main__":
+    main()
