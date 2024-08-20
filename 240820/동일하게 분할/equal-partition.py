9f9f52d4eb017e3bf2811ccee169ddb954ef187b
안상준 <69718646+sangjun19@users.noreply.github.com>
2024-08-20 10:59:48 +0900

    Title: 동일하게 분할, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240820/동일하게 분할/equal-partition.py
+++ 240820/동일하게 분할/equal-partition.py
@@ -1,16 +1,17 @@
 def main():
     n = int(input())
     arr = list(map(int, input().split()))
-    dp = [0, sum(arr)]
-    # print(dp)
-    for i in range(0, n):
-        dp[0] += arr[i]
-        dp[1] -= arr[i]
-        if dp[0] == dp[1]:
+    ans = sum(arr)
+    arr.sort()
+    for i in range(1, n):
+        result = 0
+        for j in range(0, i):
+            result += arr[j]
+        if ans - result == result:
             print("YES")
             exit()
-            
     print("NO")
+            
     
 if __name__ == "__main__":
     main()
