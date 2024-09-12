5a5a2393a4f53100fbd42f9ac247c8b17b6f13a0
안상준 <69718646+sangjun19@users.noreply.github.com>
2024-09-13 01:41:32 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 318ms, Memory: 26MB, Status: Passed - CodeTree

--- 240913/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240913/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,15 +1,13 @@
-def dfs(y, x, fy, fx, arr):
+def dfs(y, x, fy, fx, arr, visited):
+    visited.append((y, x))
     if y == fy and x == fx:
         return 1
     for dy, dx in [[1, 0], [0, 1]]:
         ny, nx = y + dy, x + dx
         if 0 <= ny < fy + 1 and 0 <= nx < fx + 1:
-            if arr[ny][nx] == 1:
-                # arr[y][x] = 0
-                finish =  dfs(ny, nx, fy, fx, arr)
-                # arr[y][x] = 1
-                if finish:
-                    return 1                                        
+            if arr[ny][nx] == 1 and (ny, nx) not in visited:
+                if dfs(ny, nx, fy, fx, arr, visited):
+                    return 1
     return 0
 
 def main():
@@ -17,7 +15,7 @@ def main():
     arr = []
     for i in range(n):
         arr.append(list(map(int, input().split())))
-    print(dfs(0, 0, n - 1, m - 1, arr))
+    print(dfs(0, 0, n - 1, m - 1, arr, []))
     
 
 if __name__ == "__main__":
