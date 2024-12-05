0b6548e993054600c521c676d7539f061a3fe3cd
안상준 <69718646+sangjun19@users.noreply.github.com>
2024-12-05 15:37:07 +0900

    Title: 사람과 와이파이, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241205/사람과 와이파이/people-and-wifi.py
@@ -0,0 +1,15 @@
+n, k = map(int, input().split())
+people = set(map(int, input().split()))
+people = list(people)
+total = 0
+dif = []
+for i in range(len(people) - 1):
+    temp = people[i + 1] - people[i]
+    total += temp
+    dif.append(temp)
+dif.sort()
+dif.reverse()
+for i in range(k - 1):
+    total -= dif[i]
+    
+print(total)
