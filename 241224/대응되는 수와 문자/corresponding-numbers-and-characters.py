4d277ee6a8e57bc69e316bcdc0cadd7ab5ae945a
안상준 <69718646+sangjun19@users.noreply.github.com>
2024-12-24 12:11:57 +0900

    Title: 대응되는 수와 문자, Time: 391ms, Memory: 47MB, Status: Passed - CodeTree

--- 241224/대응되는 수와 문자/corresponding-numbers-and-characters.py
+++ 241224/대응되는 수와 문자/corresponding-numbers-and-characters.py
@@ -4,13 +4,8 @@ d = {}
 for i in range(n):
     word = input()
     d[word] = i + 1
+    d[str(i + 1)] = word
 
 for i in range(m):
-    index = input()
-    if index.isalpha():
-        print(d[index])
-    else:
-        for k, v in d.items():
-            if v == int(index):
-                print(k)
-                break
+    index = input()    
+    print(d[index])
