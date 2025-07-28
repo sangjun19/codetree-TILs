27fa5101b00d766f086f459b3b35d32bfdaa04dc
안상준 <69718646+sangjun19@users.noreply.github.com>
2025-07-28 17:10:13 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<=i;j++) {
+            std::cout << "*";
+        }
+        std::cout << "\n";
+    }
+}
